6f93f9e1247b0b5c71d34a5485d49addcc1ead66
ffp123 <finance_ffp@163.com>
2020-06-08 09:32:36 +0800

    修改

--- Future_Price/finance_googleIndex.py
+++ Future_Price/finance_googleIndex.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     CFEARLength = 3  # 想要计算CFEAR的长度,就是想要算多少天每天的CFEAR因子值，如果以天为单位就是CFEARLength个天的CFEAR因子值。后面使用 时间长度-时间窗口 来代替它
     mat_path = "model/FuturesDataCon.mat"
     array_name = {'StockMat': ['dtes', 'rets', "settle"]}
-    google_path = '../docs/google_trends/google_trends_all_.csv'
+    google_path = '../docs/google_trends/google_indexs.csv'
     start_time = '2010-01-18'
     end_time = '2020-05-08'
     futures = ['AL','CU','RU','A','M','CF','C','B','SR','Y','TA','ZN','L','P','AU','RB','V']
